9fc877c8319b33300cbd796386a569154075ba00
Rohit <rvkillerbean@gmail.com>
2024-01-31 20:43:35 +0530

    new program

---
+++ positive-negative-zero.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter any number\n");
+    scanf("%d",&a);
+    if (a>0)
+    {
+        printf("%d is positive number",a);
+    }
+    else if (a<0)
+    {
+        printf("%d is negative number",a);
+    }
+    else if (a==0)
+    {
+        printf("%d is a zero",a);
+    }
+}
