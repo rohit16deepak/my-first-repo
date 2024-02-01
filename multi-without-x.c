52243513e07f7920b705e236ec6ac85fb8368c55
Rohit <rvkillerbean@gmail.com>
2024-02-01 19:26:35 +0530

    new p

---
+++ multi-without-x.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,sum;
+    printf("enter the value of a and b\n");
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a;i++)
+    {
+        sum+=b;
+
+    }
+    printf("%d",sum);
+
+}
